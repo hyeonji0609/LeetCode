15bd4863f150d13f1f61f89d6079fee0089b6ea5
pmingyu <mingu7539@gmail.com>
2023-11-15 12:31:23 +0900

    [MinGyu Park] 20231115_Test

---
+++ 283. Move Zeroes/Mingyu.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        
+        
+'''
+실행속도랑, 메모리 사용량
+'''
