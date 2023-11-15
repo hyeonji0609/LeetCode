8921634e33ad3d80b18c11a32d8e604be6139cf9
bmmmnmm <soccer3731@naver.com>
2023-11-15 15:38:17 +0900

    [Bongmin] 20231115_283

--- 283. Move Zeroes/Bongmin.py
+++ 283. Move Zeroes/Bongmin.py
@@ -1,7 +1,13 @@
-"""
-test
-
-test2
-"""
-
-
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        for i in nums:
+            if i == 0:        
+                nums.append(nums.pop(nums.index(0)))
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        """
+        runtime : 766 ms
+        memory : 17.8 MB
+        """
