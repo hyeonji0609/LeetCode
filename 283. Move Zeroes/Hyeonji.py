0b303b6543c9fde53b5d3ba05baa35507c422972
hyeonji0609 <hj.jeong0609@gmail.com>
2023-11-16 13:41:23 +0900

    [Hyeonji Jeong] 20231116_283

--- 283. Move Zeroes/Hyeonji.py
+++ 283. Move Zeroes/Hyeonji.py
@@ -1,1 +1,18 @@
-Test
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) == 1:
+            return nums
+        else :
+            sum_zero = nums.count(0)
+            for i in nums[:]:
+                if i == 0:
+    	            nums.remove(0)
+            nums.extend([0] * sum_zero)
+
+"""
+Runtime: 447 ms
+Memory Usage: 18 MB
+"""
