983291f5094160e0fea1876e7597eecad9e9f22b
pmingyu <mingu7539@gmail.com>
2023-11-17 20:09:16 +0900

    [Mingyu Park] 20231117_283

--- 283. Move Zeroes/Mingyu.py
+++ 283. Move Zeroes/Mingyu.py
@@ -3,8 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
+        zero_count = nums.count(0)
+        zero_list = []
+        for i in range(int(zero_count)):
+            zero_list.append(0)
+            nums.remove(0)
+        nums.extend(zero_list)
         
         
 '''
+Runtime: 471 ms - Beats 13.75%
+Memory: 17.96MB - Beats 13.36%
 '''
