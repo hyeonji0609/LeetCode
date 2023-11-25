f6f56ebf9aebd8d669562c46c4c70e558252e88c
bmmmnmm <soccer3731@naver.com>
2023-11-25 15:34:07 +0900

    [Bongmin] 20231125_724

--- 724. Find Pivot Index/Bongmin.py
+++ 724. Find Pivot Index/Bongmin.py
@@ -1,10 +1,14 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        n = 0
-        for i in range(len(nums)):
-            if sum(nums[:i]) == sum(nums[i+1:]):
-                n = i
+        n = 0 # 인덱스를 저장할 초기값
+        for i in range(len(nums)): # 리스트를 돌아서
+            if sum(nums[:i]) == sum(nums[i+1:]): # 양쪽으로 나눈 것들의 합이 같으면
+                n = i # 그 인덱스를 반환
                 break
             else:
-                n = -1
+                n = -1 # 없으면 -1
         return n
+'''
+runtime : 6595 ms
+memory : 17.5 mb
+'''
