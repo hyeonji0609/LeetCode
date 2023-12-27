eb7105db294c76378f99908db9c9ffff17a944bd
pmingyu <mingu7539@gmail.com>
2023-12-27 18:10:57 +0900

    [Mingyu Park] git_test

--- 6_2130. Maximum Twin Sum of a Linked List/Mingyu.py
+++ 6_2130. Maximum Twin Sum of a Linked List/Mingyu.py
@@ -3,6 +3,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+        
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
         # 연결리스트 길이 조회 함수
