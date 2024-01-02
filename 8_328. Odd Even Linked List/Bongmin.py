980250353125576dc434349591c455b6e2a444a7
bmmmnmm <soccer3731@naver.com>
2024-01-02 22:43:56 +0900

    [Bongmin] 20240102_328

--- 8_328. Odd Even Linked List/Bongmin.py
+++ 8_328. Odd Even Linked List/Bongmin.py
@@ -3,7 +3,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
+# 1 리스트를 3개 만들어서 이게 공간복잡도가 어떻게 되는지 잘모르겠습니다 ..!
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # 비어있는 경우를 제외
@@ -42,4 +42,50 @@ class Solution:
 runtime : 96ms 상위 95% ...
 memory : 19.9 mb # 하위 95%............
 런타임도 안좋고 메모리도 안좋다
+'''
+
+# 2 O(1) 1개만 추가로 허용한다 해서 stack 하나만 가지고 풀었습니다.
+
+import math
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # 비어있는 경우를 제외
+        if not head:
+            return head
+        
+        stack = []
+
+        while head:
+            stack.append(head.val)  # 현재 값을 링크드 리스트에 넣기
+            head = head.next # 다음 것에 연결하기
+
+        top = ListNode(stack[0])
+        current = top
+        # a.pop(안에 인덱스)
+        len_stack = len(stack)
+        for_iter = math.ceil(len_stack / 2)
+        for i in range(for_iter):
+            if i == 0:
+                stack.pop(i)
+                continue
+            else:
+                current.next = ListNode(stack[i])
+                current = current.next
+                stack.pop(i)
+        for i in range(len(stack)):
+            current.next = ListNode(stack[i])
+            current = current.next
+
+        return top
+
+
+'''
+runtime : 48ms 상위 49%!!!
+memory : 19.5 mb # 하위 95% 그대로 .........
+런타임은 아주 많이 개선!
 '''
