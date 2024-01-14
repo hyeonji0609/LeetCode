99bae954815688a280343d23e7c65164294bef39
bmmmnmm <soccer3731@naver.com>
2024-01-14 12:41:52 +0900

    [Bongmin] 20240114_392

--- 2_392. Is Subsequence/Bongmin.py
+++ 2_392. Is Subsequence/Bongmin.py
@@ -12,3 +12,24 @@ class Solution:
         except:
             return False
 
+
+###########
+# 오답노트
+###########
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = 0
+        j = 0
+        cnt = len(s)
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i+=1
+            j+=1
+        if cnt == i:
+            return True
+        else:
+            return False
+'''
+runtime : 35ms : beats 79.57%
+memory : 17.34MB : beats 29.95%
+'''
