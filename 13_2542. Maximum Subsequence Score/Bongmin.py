a66473d24aa0f86e2bc768ae4b189c50cbaf3ba1
bmmmnmm <soccer3731@naver.com>
2024-02-02 12:05:55 +0900

    [Bongmin] 20240202_2336,2542

---
+++ 13_2542. Maximum Subsequence Score/Bongmin.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        idx_ls = [x for x in range(len(nums1))]
+        comb_ls = list(combinations(idx_ls, r=k))
+        res = 0
+        for comb in comb_ls:
+            sum_nums1 = sum([nums1[x] for x in comb])
+            min_nums2 = min([nums2[x] for x in comb])
+            total = sum_nums1 * min_nums2
+            if total > res:
+                res = total
+        return res
+# 메모리 에러
+
