819799d3591ed741f2b4469ad1ddb58b0c7a56ea
bmmmnmm <soccer3731@naver.com>
2024-02-07 15:01:49 +0900

    [Bongmin] 20240207_2542

--- 13_2542. Maximum Subsequence Score/Bongmin.py
+++ 13_2542. Maximum Subsequence Score/Bongmin.py
@@ -1,7 +1,7 @@
 from itertools import combinations
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        idx_ls = [x for x in range(len(nums1))]
+        idx_ls = [x for x in range(len(nums1))] 
         comb_ls = list(combinations(idx_ls, r=k))
         res = 0
         for comb in comb_ls:
@@ -13,3 +13,21 @@ class Solution:
         return res
 # 메모리 에러
 # 아직 못풀었습니다 ㅠ
+
+import heapq
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        res = 0
+        for comb in list(combinations([x for x in range(len(nums1))], r=k)):
+            sum_nums1 = 0 
+            temp = []
+            for i in comb:
+                sum_nums1 += nums1[i]
+                heapq.heappush(temp, nums2[i])
+            min_nums2 = temp[0]
+            total = sum_nums1 * min_nums2
+
+            if total > res:
+                res = total
+        return res
+
