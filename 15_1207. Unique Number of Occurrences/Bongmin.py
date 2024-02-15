d09fc723e43bd6538aea87ed4df97f6398828464
bmmmnmm <soccer3731@naver.com>
2024-02-15 12:39:55 +0900

    [Bongmin] 20240215_1207

---
+++ 15_1207. Unique Number of Occurrences/Bongmin.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        digit_dict = {}
+        for i in arr:
+            if i not in digit_dict:
+                digit_dict[i] = 1
+            else:
+                digit_dict[i] +=1
+        # 고유값을 카운트를 해서 같으면 유니크하다는 뜻이므로 True
+        if len(set(digit_dict.values())) ==len(digit_dict.values()):
+            return True
+        else:
+            return False
