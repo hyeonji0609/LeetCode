b3b625bce6b6d6b0f449a783a3cce02f172073cb
bmmmnmm <soccer3731@naver.com>
2024-04-01 17:26:36 +0900

    [Bongmin] 20240401_199

--- 17_199. Binary Tree Right Side View/Bongmin_revenge.py
+++ 17_199. Binary Tree Right Side View/Bongmin_revenge.py
@@ -11,9 +11,9 @@ class Solution:
         def dfs(node, depth):
             if node:
                 # 깊이가 지금까지 들어온 ans랑 같으면 그 노드를 추가
-                if depth == len(ans): # 처음 ans의 길이는 0
+                if depth == len(ans): # 처음 ans의 길이는 0 
                     ans.append(node.val)
-                # 깊이는 결국 마지막 리프노드가 될 것 이기 때문
+                # right를 먼저 돌게 됨으로써 index와 depth를 맞춰줄 수 있다.
                 dfs(node.right, depth+1)
                 dfs(node.left, depth+1)
         ans = []
