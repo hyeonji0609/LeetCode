d74cf03f8346aa874d918b1e9140efb29110853e
bmmmnmm <soccer3731@naver.com>
2024-04-11 17:57:43 +0900

    [Bongmin] 20240411_162,2300,257,374

---
+++ 23_374. Guess Number Higher or Lower/Bongmin.py
@@ -0,0 +1,21 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        start = 1 # 시작
+        end = n # 끝
+        while start<=end:
+            mid = (start + end) // 2
+            if guess(mid) == 1: # 높여라 -> start를 높여서 new_mid를 높여라
+                start = mid + 1
+            elif guess(mid) == -1: # 낮춰라 -> end를 낮춰서 new_mid를 낮춰라
+                end = mid - 1 
+            elif guess(mid) == 0: # 맞췄다
+                return mid
+        return None
+
