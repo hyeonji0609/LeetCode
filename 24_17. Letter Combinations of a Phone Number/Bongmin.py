6d10338808f75cd83cfc8223b05ce2c929e80b9b
bmmmnmm <soccer3731@naver.com>
2024-04-17 13:55:31 +0900

    [Bongmin] Change_week

--- 24_17. Letter Combinations of a Phone Number/Bongmin.py
+++ 24_17. Letter Combinations of a Phone Number/Bongmin.py
@@ -15,13 +15,11 @@ class Solution:
         }
         # 결과
         res = []
-
         def back(text, digits):
             # 남은 숫자가 없다면 그 문자를 넣고 종료
             if not digits:
                 res.append(text)
                 return
-
             # 남은 숫자의 1번째 값에 해당하는 alpha들 가져오기
             for alpha in nums_alpha[digits[0]]:
                 # text에 들어온 alpha를 추가하고 숫자도 한칸 띄어 넣기
